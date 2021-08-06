e891586c2fa4e813ecd7de053a508463877e7476
moongoo814 <75865580+moongoo814@users.noreply.github.com>
2021-08-06 15:31:55 +0900

    Create valid-palindrome.py

---
+++ valid-palindrome.py
@@ -0,0 +1,12 @@
+def isPalindrome(self, s:str) -> bool:
+    strs: Deque = collections.deque()
+
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+    
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+    
+    return True
